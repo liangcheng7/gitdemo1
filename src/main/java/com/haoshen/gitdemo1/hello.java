cb867ea7b0d390d6d30dc99ed5bcf3af4ad1dc66
haoshen <2935723852@qq.com>
2022-02-14 18:05:01 +0800

    Second commit

--- src/main/java/com/haoshen/gitdemo1/hello.java
+++ src/main/java/com/haoshen/gitdemo1/hello.java
@@ -3,5 +3,6 @@ package com.haoshen.gitdemo1;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("第二次提交！");
     }
 }
